1d7f28194e8f176f88150f1b6c1ec8bf945eede5
Vadim Kuchukov <kuchukov.95@mail.ru>
2023-05-23 18:25:45 +0300

    Dog

---
+++ Dog.java
@@ -0,0 +1,16 @@
+public class Dog {
+    String name;
+
+    Dog(String name) {
+        this.name = name;
+    }
+
+    public void jump(int count) {
+        System.out.println("Пёс " + name + " прыгнул " + count + " раз.");
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Барбос");
+        dog.jump(7);
+    }
+}
